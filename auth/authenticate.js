cc9d3addce8a1505dd8df432eeaadb86c25bf5b5
ismaponto <isma.pontoriero@gmail.com>
2023-11-16 15:23:58 +0100

    no .env

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,5 +1,5 @@
 const getTokenFromHeader = require('./getTokenfromHeader');
-const { jsonResponse } = require('../auth/jsonResponse');
+const { jsonResponse } = require('../lib/jsonResponse.js');
 const { verifyAccessToken } = require('./verifyTokens');
 
 function authenticate(req, res, next) {
